03db509e2ebbcc70c5d5805c6d4d6050583f9a9d
오이삭 <issac1162@cowave.kr>
2025-08-26 20:37:28 +0900

    fix: path 수정

--- next.config.ts
+++ next.config.ts
@@ -6,8 +6,6 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/Back4Sac2.github.io' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/Back4Sac2.github.io' : '',
 };
 
 export default nextConfig;
